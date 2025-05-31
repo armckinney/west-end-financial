3844233c03e4496557e24beaada4cc306a3e9781
armck <armck.phantom@gmail.com>
2025-05-31 05:39:55 +0000

    fix cae cert

--- terraform/configurations/acmebot/main.tf
+++ terraform/configurations/acmebot/main.tf
@@ -7,3 +7,15 @@ terraform {
     }
   }
 }
+
+# todo: app role auth & dns config
+# - https://github.com/shibayan/keyvault-acmebot/wiki/App-Role-based-authorization
+# - https://github.com/shibayan/keyvault-acmebot/wiki/DNS-Provider-Configuration
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
--- terraform/modules/containerapp_environment_certificate/cae_certificate.tf
+++ terraform/modules/containerapp_environment_certificate/cae_certificate.tf
@@ -2,7 +2,7 @@
 # add certificate to the container app environment
 resource "azurerm_container_app_environment_certificate" "this" {
   name                         = "cert-${var.application}-${var.environment}"
-  container_app_environment_id = var.azurerm_container_app_environment.id
+  container_app_environment_id = var.containerapp_environment.id
   certificate_blob_base64      = acme_certificate.this.certificate_p12
   certificate_password         = acme_certificate.this.certificate_p12_password
   tags                         = var.tags
